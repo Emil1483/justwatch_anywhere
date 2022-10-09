c184ef229efcaf76e6c1e7e9060bed17fa0a0535
Emil Djupvik <emil@djupvik.dev>
2022-10-09 20:53:29 +0200

    random commit

--- server/index.py
+++ server/index.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-    return 'Hello World 👩'
+    return 'Hello World 🍕'
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=8080, debug=True)
